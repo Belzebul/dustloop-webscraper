a2866087a7e5e28b4a1ef7d2900436d3e8782570
Capiroto <renato2520@gmail.com>
2021-08-24 11:19:17 -0300

    correção de tratamente de exceções

--- src/character_html_collector.py
+++ src/character_html_collector.py
@@ -4,17 +4,23 @@ from bs4.element import Tag
 import requests
 
 HTTP_OK = 200
+HEADERS = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)'
+                          'AppleWebKit/537.36 (KHTML, like Gecko)'
+                          'Chrome/45.0.2454.101 Safari/537.36'),
+                          'referer': 'https://www.dustloop.com/wiki/index.php?title=GGST/Frame_Data'}
 
 class CharacterHTMLCollector:
 
     def __init__(self, url_character: str):
+
         http_content: bytes = self.load_data(url_character).content
+
         self.tbody_list = self.parse_tables(http_content, 'tbody')
         self.thead_list = self.parse_tables(http_content, 'thead')
 
 
     def load_data(self, URL):
-        http_request = requests.get(URL)
+        http_request = requests.get(URL, headers=HEADERS)
         if( http_request.status_code != HTTP_OK):
             raise RequestException(URL)
 
@@ -42,6 +48,8 @@ class RequestException(Exception):
 
 
 if __name__ == '__main__':
-    test_collector = CharacterHTMLCollector('https://www.dustloop.com/wiki/index.php?title=GGST/Nagoriyuki/Frame_Data')
-    with open('html_test_specials.html', 'w') as file:
-        file.write(test_collector.get_tbody(2).prettify())
+    url = 'https://www.dustloop.com/wiki/index.php?title=GGST/Nagoriyuki/Frame_Data'
+    test_collector = CharacterHTMLCollector(url)
+    print(test_collector.get_tbody(2).prettify())
+    '''with open('html_test_specials.html', 'w') as file:
+        file.write(test_collector.get_tbody(2).prettify())'''
